8d67f4f33102f07287d37a794ef0dec7bcfc7f8d
Eric Jorgensen <ericjorg@thejcrew.net>
2021-06-18 12:10:54 -0700

    Initial checkin

---
+++ AppModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appmeiser
+{
+    class AppModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ProcessModel> _processes = new ObservableCollection<ProcessModel>();
+        public ObservableCollection<ProcessModel> Processes => _processes;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // ------------------------------------------------------------------------------------------
+        // ctor
+        // ------------------------------------------------------------------------------------------
+        public AppModel()
+        {
+            var lookup = GetProcessInstanceIdLookup();
+            foreach (var process in Process.GetProcesses())
+            {
+                var instanceName = process.ProcessName;
+                if(lookup.ContainsKey(process.Id))
+                {
+                    instanceName = lookup[process.Id];
+                }
+                Processes.Add(new ProcessModel(process, lookup));
+
+            }
+        }
+
+        // ------------------------------------------------------------------------------------------
+        // ctor
+        // ------------------------------------------------------------------------------------------
+        Dictionary<int, string> GetProcessInstanceIdLookup()
+        {
+            var output = new Dictionary<int, string>();
+            var cat = new PerformanceCounterCategory("Process");
+
+            string[] instances = cat.GetInstanceNames();
+            foreach (string instanceName in instances)
+            {
+
+                try
+                {
+                    using (PerformanceCounter cnt = new PerformanceCounter("Process",  "ID Process", instanceName, true))
+                    {
+                        int val = (int)cnt.RawValue;
+                        output[val] = instanceName;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error while checking instance name: " + e.Message);
+                }
+            }
+            return output;
+        }
+        
+    }
+}
---
+++ ProcessModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ComType = System.Runtime.InteropServices.ComTypes;
+
+namespace appmeiser
+{
+    class ProcessModel : INotifyPropertyChanged
+    {
+        public string Name => _process.ProcessName;
+        public int Id => _process.Id;
+        public string InstanceName
+        {
+            get
+            {
+                if (_instanceNameLookup.TryGetValue(_process.Id, out var name)) return name;
+                else return "*killed*";
+            }
+        }
+        public string CommandLine => _process.StartInfo.FileName + " " + _process.StartInfo.Arguments?.ToString();
+
+        public double Cpu { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Timer _processCheck;
+
+        private Process _process;
+        private Dictionary<int, string> _instanceNameLookup;
+        PerformanceCounter _cpu;
+
+        // ------------------------------------------------------------------------------------------
+        // ctor
+        // ------------------------------------------------------------------------------------------
+        public ProcessModel(Process process, Dictionary<int, string> instanceNameLookup)
+        {
+            this._process = process;
+            this._instanceNameLookup = instanceNameLookup;
+
+            var instanceName = InstanceName;
+            if(instanceName != "*killed*")
+            {
+                _cpu = new PerformanceCounter("Process", "% Processor Time", instanceName, true);
+                this._processCheck = new Timer(timerTick, null, 0, 1000);
+            }
+        }
+
+
+        // ------------------------------------------------------------------------------------------
+        // timerTick
+        // ------------------------------------------------------------------------------------------
+        void timerTick(object state)
+        {
+            try
+            {
+                Cpu += ((int)(_cpu.NextValue() * 1000))/1000.0;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("Measurement error: " + e.Message);
+            }
+            NotifyPropertyChanged(nameof(Cpu));
+
+        }
+
+        // ------------------------------------------------------------------------------------------
+        // NotifyPropertyChanged
+        // ------------------------------------------------------------------------------------------
+        void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
